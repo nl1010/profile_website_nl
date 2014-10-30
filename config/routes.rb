ca5fd6073d07b49113f1b5ae8eeca33ff3a57263
nl1010 <yanlu1990@hotmail.co.uk>
2014-10-30 09:35:28 +0000

    Main page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'main/home'
+
   resources :internships
 
   resources :articles
@@ -8,6 +10,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'main#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
